7609ff60e3988d733f2f58741f5ffc8a81326923
이지영 <xkagja2006@naver.com>
2023-03-10 13:53:25 +0900

    0310

---
+++ 05-FrontEnd/workshop/front_hw_03_2/js/main.js
@@ -0,0 +1,21 @@
+// register.html 의 form 에 있는 버튼을 눌렀을때 실행되는 함수
+function regist() {
+  // 각 form 의 input 에 입력된 값들을 css 선택자를 이용하여 가져오기
+  let id = document.querySelector('#id');
+  let password = document.querySelector('#password');
+  let name = document.querySelector('#name');
+  let email = document.querySelector('#email');
+  let age = document.querySelector('#age');
+
+  // user 생성
+  const user = {
+    id: id,
+    password: password,
+    name: name,
+    email: email,
+    age: age,
+  };
+
+  // user 정보 출력
+  console.log(JSON.stringify(user));
+}
