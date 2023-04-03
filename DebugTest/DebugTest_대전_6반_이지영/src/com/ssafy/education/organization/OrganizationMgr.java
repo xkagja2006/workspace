8f63e4b0714c8d07d7f3756fea2393e4a62e1570
이지영 <xkagja2006@naver.com>
2023-04-03 10:52:44 +0900

    0403

--- DebugTest/DebugTest_대전_6반_이지영/src/com/ssafy/education/organization/OrganizationMgr.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.education.organization;
-
-import java.util.List;
-
-public interface OrganizationMgr {
-	List<Organization> search();
-	Organization search(Organization o);
-}
--- DebugTest/DebugTest_대전_6반_이지영2/src/com/ssafy/education/subject/SubjectsMgr.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.education.subject;
-
-public interface SubjectsMgr {
-	void add(Subject v) throws DuplicatedException;
-	Subject[] search();
-	Subject search(String name) throws NotFoundException, DuplicatedException;
-}
