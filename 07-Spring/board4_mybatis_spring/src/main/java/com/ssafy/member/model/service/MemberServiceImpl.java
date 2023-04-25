f98a324748d649dae8ea73bfa9a32759b465af28
이지영 <xkagja2006@naver.com>
2023-04-25 15:55:38 +0900

    0425

---
+++ 07-Spring/board4_mybatis_spring/src/main/java/com/ssafy/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ssafy.member.model.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.member.model.MemberDto;
+import com.ssafy.member.model.mapper.MemberMapper;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+	
+//	@Autowired
+//	private SqlSession sqlSession;
+	
+	private MemberMapper memberMapper;
+
+	public MemberServiceImpl(MemberMapper memberMapper) {
+		super();
+		this.memberMapper = memberMapper;
+	}
+
+	@Override
+	public int idCheck(String userId) throws Exception {
+//		return sqlSession.getMapper(MemberMapper.class).idCheck(userId);
+		return memberMapper.idCheck(userId);
+	}
+
+	@Override
+	public void joinMember(MemberDto memberDto) throws Exception {
+//		sqlSession.getMapper(MemberMapper.class).joinMember(memberDto);
+		memberMapper.joinMember(memberDto);
+	}
+
+	@Override
+	public MemberDto loginMember(Map<String, String> map) throws Exception {
+//		return sqlSession.getMapper(MemberMapper.class).loginMember(map);
+		return memberMapper.loginMember(map);
+	}
+
+}
